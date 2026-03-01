2b01ded5e606e82b4a6a18d3f5008fd5a4cac49f
soogoino <soogoino@proton.me>
2026-03-01 22:48:18 +0800

    chore: bump versionCode=4 versionName=1.0.3; add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.chastity.diary"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
